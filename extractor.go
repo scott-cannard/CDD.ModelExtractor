3f91694e4cd12798e7607290ea5d8e70103de0de
unknown <scott.cannard@hotmail.com>
2016-03-09 12:40:54 -0800

    prototype first draft complete

--- extractor.go
+++ extractor.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -13,25 +12,25 @@ func isError(e error) bool {
 	if e != nil {
 		panic(e)
 	}
-	return true
+	return false
 }
 
 func main() {
 	//Validate command line args
 	if len(os.Args) < 2 {
-		panic("Usage: extractor <model.json filepath>")
-	}
+		fmt.Println("Usage: extractor <model.json filepath>")
+	} else {
+		//UI component will write model.json file
+		//Get filepath from command line args
+		fmt.Println("Reading " + os.Args[1])
+		dat, err := ioutil.ReadFile(os.Args[1])
 
-	//UI component will write model.json file
-	//Get filepath from command line args
-	fmt.Println("Reading " + os.Args[1])
-	modelJson, err := ioutil.ReadFile(os.Args[1])
+		if !isError(err) {
+			//Send to SchemaBuilder as HTTP POST request
+			modelJson := bytes.NewReader(dat)
 
-	if !isError(err) {
-		//Send to SchemaBuilder as HTTP POST request
-		b := bytes.NewBufferString(modelJson)
-
-		fmt.Println("Sending model to Schema Builder")
-		http.Post("http://127.0.0.1:8081/", "text/json", b)
+			fmt.Println("Sending model to Schema Builder")
+			http.Post("http://127.0.0.1:8081/", "text/json", modelJson)
+		}
 	}
 }
